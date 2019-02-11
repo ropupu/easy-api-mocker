01156d8f828f082d2d9d68135328ed4fa5f6aedf
ropupu <ropupu.ropupu@gmail.com>
2019-02-11 15:43:38 +0900

    add endpoint delete

--- application/src/server.ts
+++ application/src/server.ts
@@ -2,6 +2,7 @@ import * as Express from "express";
 import * as bodyParser from "body-parser";
 import { CreateGroupUsecase } from 'usecases/group/CreateGroupUsecase';
 import { CreateEndpointUsecase } from 'usecases/endpoint/CreateEndpointUsecase';
+import { DeleteEndpointUsecase } from 'usecases/endpoint/DeleteEndpointUsecase';
 import { GetEndpointsUsecase } from 'usecases/endpoint/GetEndpointsUsecase';
 import { GroupRepository } from 'interfaces/repositories/GroupRepository';
 import { GroupsRepository } from 'interfaces/repositories/GroupsRepository';
@@ -69,7 +70,13 @@ app.post('/api/:group_key', async (req: Express.Request, res: Express.Response) 
   res.status(200).send({});
 })
 
-app.delete('/api/:group_key')
+app.delete('/api/:group_key', async (req: Express.Request, res: Express.Response) => {
+  const endpointKey = req.body.endpoint_key;
+  const endpointsRepository = new EndpointsRepository();
+  const deleteEndpointUsecase = new DeleteEndpointUsecase(endpointsRepository);
+  await deleteEndpointUsecase.normal(endpointKey);
+  res.status(200).send({});
+})
 
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
